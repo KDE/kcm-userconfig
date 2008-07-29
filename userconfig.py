5d2191e6c925f73dcbf4de2a1ae0fb57d73dc63e
Yuriy Kozlov <yuriy.kozlov@gmail.com>
2008-07-29 15:16:45 -0400

    Moved some backend stuff into util and made init files.

--- userconfig.py
+++ userconfig.py
@@ -4,7 +4,7 @@
 # userconfig.py - description                                             #
 # ------------------------------                                          #
 # begin     : Wed Apr 30 2003                                             #
-# copyright : (C) 2003-2006 by Simon Edwards                              #
+# copyright : (C) 2003-2006 by Simon Edwards, 2008 by Yuriy Kozlov        #
 # email     : simon@simonzone.com                                         #
 #                                                                         #
 ###########################################################################
@@ -28,7 +28,8 @@ from PyKDE4.kio import *
 import sys
 import os.path
 import shutil
-import unixauthdb
+from util import unixauthdb
+from util.groups import *   # TODO: Get rid of * imports
 import locale
 
 programname = "userconfig"
@@ -611,31 +612,6 @@ class UserConfigApp(programbase):
         return KCModule.Help
 
 ###########################################################################
-
-# Rudd-O convenience class to map groups to privilege names
-class PrivilegeNames(dict):
-    """Convenience dict-derived class: map known secondary groups to privilege names, provide default mapping for groups that do not have a description.  This could be replaced by a simple dict() but I simply preferred the class declaration.
-    
-    FIXME This should ideally be included in a more general module so it can be reused."""
-
-    def __init__(self):
-        dict.__init__(self, {
-            "plugdev":i18n("Access external storage devices automatically"),
-            "adm":i18n("Administer the system"),
-            "ltsp":i18n("Allow use of FUSE filesystems like LTSP thin client block devices"),
-            "dialout":i18n("Connect to the Internet using a modem"),
-            "syslog":i18n("Monitor system logs"),
-            "fax":i18n("Send and receive faxes"),
-            "cdrom":i18n("Use CD-ROM and DVD drives"),
-            "floppy":i18n("Use floppy drives"),
-            "modem":i18n("Use modems"),
-            "scanner":i18n("Use scanners"),
-        })
-
-    def __getitem__(self,name):
-        # This is cruft but I couldn't bring myself to kill it bua!
-        if name in self: return dict.__getitem__(self,name)
-        return i18n("Be a member of the %s group")%name
 
 class UserEditDialog(KPageDialog):
     def __init__(self,parent,admincontext):
