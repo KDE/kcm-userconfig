9d52b4405d03eaf144f09263348e8a35e994eb90
Jonathan Thomas (The man) <echidnaman@gmail.com>
2009-09-02 10:10:15 -0400

    Add missing cmake-modules directory to try to help prevent the installaion of pyc files

---
+++ cmake-modules/FindPyKDE.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2008, Jonathan Riddell <jriddell@ubuntu.com>
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+try:
+    from PyKDE4.kdeui import KApplication
+except:
+    exit(1)
+print "Groovy"
