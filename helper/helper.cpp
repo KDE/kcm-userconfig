8c713dc98cddfe55d5aa11b0ebf01c294f190f87
Romain Perier <romain.perier@gmail.com>
2011-05-12 19:50:11 +0200

    Update the helper licenses headers corresponding to the licensing policy

--- helper/helper.cpp
+++ helper/helper.cpp
@@ -1,13 +1,20 @@
 /*
- * A KCModule for user and group configuration
+ * Copyright 2011  Romain Perier <romain.perier@gmail.com>
  *
- * Copyright 2011 Romain Perier.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
  *
- * Authors:
- * - Romain Perier <romain.perier@gmail.com>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * License: GPL v3
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "helper.h"
 
 #include <unistd.h>
